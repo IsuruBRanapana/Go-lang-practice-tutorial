bb5d2ddd1282562ce147b44e38440572e416b05b
Isuru B. Ranapana <raaisurub.ranapana@gmail.com>
2020-07-20 20:42:08 +0530

    Start to learn Go, Variables are learned

---
+++ tutorial.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Hello GO"
+	var name2 string
+	name2 = "Charuni"
+	name2 = "Isuru"
+	var number uint16 = 260
+	fmt.Println("Hello world", name, name2, number)
+}
