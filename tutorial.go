22d1af5ae55bd11d275d8da8502d0487c0ca0afb
Isuru B. Ranapana <raaisurub.ranapana@gmail.com>
2020-07-21 19:28:36 +0530

    printing and formatting fmt module

--- tutorial.go
+++ tutorial.go
@@ -27,10 +27,31 @@ func main() {
 	fmt.Printf("\n Number : %x", 3435)
 	//hexa in capital letter
 	fmt.Printf("\n Number : %X", 3435)
+	//padding
+	fmt.Printf("\n Number Pad: %09d", 3435)
 
 	//floating points
 	fmt.Printf("\n Number : %e", 3435.15456231545132)
 	fmt.Printf("\n Number : %f", 3435.15456231545132)
 	fmt.Printf("\n Number : %F", 3435.15456231545132)
 	fmt.Printf("\n Number : %g", 3435.15456231545132)
+	//width and percision
+	fmt.Printf("\n Number : %9f", 3435.15456231545132)
+	fmt.Printf("\n Number : %.2f", 3435.15456231545132)
+	fmt.Printf("\n Number : %9.2f", 3435.15456231545132)
+	fmt.Printf("\n Number : %9.f", 3435.15456231545132)
+	//padding
+	fmt.Printf("\n Number Pad: %09f", 3435.15456231545132)
+
+	//String formatting
+	fmt.Printf("\n Name : %s", "Isuru")
+	fmt.Printf("\n Name : %q", "Isuru")
+	//width and percision
+	fmt.Printf("\n Name : %9s is cool", "Isuru")
+	fmt.Printf("\n Name : %-9s is cool", "Isuru")
+
+	//Sprint (store print fmt value in variable
+	var x string = fmt.Sprintf("\n Number Pad: %09f", 3435.15456231545132)
+	fmt.Println(x)
+
 }
