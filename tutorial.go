39d21fa3b3c15b0a962ffb40e61ad2c981597f46
Isuru B. Ranapana <raaisurub.ranapana@gmail.com>
2020-07-21 18:59:06 +0530

    Learn implicit and explicit variables

--- tutorial.go
+++ tutorial.go
@@ -8,5 +8,14 @@ func main() {
 	name2 = "Charuni"
 	name2 = "Isuru"
 	var number uint16 = 260
+	var num = 230
+	numEasy := 520
+	fmt.Printf("%T", numEasy)
+	fmt.Printf("%T", num)
 	fmt.Println("Hello world", name, name2, number)
+	//all variables have default val
+	var defnum uint64
+	var bl bool
+	fmt.Println(defnum, bl)
+
 }
