4b800974aa1db91e5162de1a8f785a689380dad9
Isuru B. Ranapana <raaisurub.ranapana@gmail.com>
2020-07-21 19:14:53 +0530

    floating point formatting

--- tutorial.go
+++ tutorial.go
@@ -20,8 +20,17 @@ func main() {
 
 	//integer format
 	//get binary
-	fmt.Printf("\n Number : %b", 1152)
-	fmt.Printf("\n Number : %o", 1152)
-	fmt.Printf("\n Number : %d", 1152)
-	fmt.Printf("\n Number : %x", 1152)
+	fmt.Printf("\n Number : %b", 3435)
+	fmt.Printf("\n Number : %o", 3435)
+	fmt.Printf("\n Number : %d", 3435)
+	//hexa in simple letter
+	fmt.Printf("\n Number : %x", 3435)
+	//hexa in capital letter
+	fmt.Printf("\n Number : %X", 3435)
+
+	//floating points
+	fmt.Printf("\n Number : %e", 3435.15456231545132)
+	fmt.Printf("\n Number : %f", 3435.15456231545132)
+	fmt.Printf("\n Number : %F", 3435.15456231545132)
+	fmt.Printf("\n Number : %g", 3435.15456231545132)
 }
